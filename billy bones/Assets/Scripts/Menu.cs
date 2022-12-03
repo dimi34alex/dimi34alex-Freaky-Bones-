abeb144e6222c469489892034415f44932b69c77
FandXOfficial <90962224+FandXOfficial@users.noreply.github.com>
2022-12-03 20:07:17 +0300

    ялквмом
    новая менюшка и пауза на esc

--- billy bones/Assets/Scripts/Menu.cs
+++ billy bones/Assets/Scripts/Menu.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class Menu : MonoBehaviour
 {
     public GameObject MainMenu, Settings, Loading;
     public bool Quit_Press = false, Pause_Press = false;
+    private Animator anim;
 
     public void MainMenu_f()
     {
         MainMenu.SetActive(true);
         Settings.SetActive(false);
         Loading.SetActive(false);
+        anim.SetBool("IsRunning", false);
     }
 
     public void Settings_f()
@@ -27,6 +30,7 @@ public class Menu : MonoBehaviour
         MainMenu.SetActive(false);
         Settings.SetActive(false);
         Loading.SetActive(true);
+        anim.SetBool("IsRunning", true);
         Invoke("ActivationMainScene", 5f);
     }
 
@@ -42,7 +46,7 @@ public class Menu : MonoBehaviour
 
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     void Update()
--- billy bones/Assets/Scripts/MenuPauseScript.cs
+++ billy bones/Assets/Scripts/MenuPauseScript.cs
@@ -1,35 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPauseScript : MonoBehaviour
 {
-    public GameObject PauseMenu, PauseSettings, PauseButton;
-    public bool Quit_Press = false, Pause_Press = false;
-    public float timespeed;
+    public GameObject PauseMenu, PauseSettings;
+    public bool Quit_Press = false;
+    public float timespeed = 1f;
 
     public void PauseMenu_f()
     {
-        PauseButton.SetActive(false);
         PauseMenu.SetActive(true);
         PauseSettings.SetActive(false);
-        Pause_Press = true;
     }
 
     public void PauseSettings_f()
     {
-        PauseButton.SetActive(false);
         PauseMenu.SetActive(false);
         PauseSettings.SetActive(true);
     }
 
     public void PauseContinue_f()
     {
-        PauseButton.SetActive(true);
         PauseMenu.SetActive(false);
         PauseSettings.SetActive(false);
-        Pause_Press = false;
+        timespeed = 1f;
     }
+
+/*    public void PauseToMainMenu_f()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }*/
 
     public void Quit_Button()
     {
@@ -38,19 +40,17 @@ public class MenuPauseScript : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         Time.timeScale = timespeed;
-        if (Pause_Press)
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
+            PauseMenu.SetActive(true);
             timespeed = 0f;
-        }
-        else
-        {
-            timespeed = 1f;
         }
 
         if (Quit_Press)
