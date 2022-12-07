ac6dab89ccac8259452db83167fb70eb2e02f6a9
Adam <adam.butakov@yandex.ru>
2022-12-07 19:16:08 +0300

    Теперь можно брать некоторые предметы
    жопа жопа какашкииии

--- billy bones/Assets/Scripts/Interactive.cs
+++ billy bones/Assets/Scripts/Interactive.cs
@@ -16,6 +16,9 @@ public class Interactive : MonoBehaviour
         Ray();
         DrawRay();
         Interact();
+        OutLining();
+        if (Input.GetKeyDown(KeyCode.E)) Pick_Item();
+        if (Input.GetKeyDown(KeyCode.Q)) Drop();
     }
 
     private void Ray()
@@ -32,17 +35,12 @@ public class Interactive : MonoBehaviour
     }
 
     public GameObject DIMON;
-    private Outline outline;
-    public GameObject Rich1;
-    public GameObject Rich2;
 
     private void Interact()
     {
         if(_hit.transform != null && _hit.transform.GetComponent<Richag1>())
         {
             Debug.DrawRay(_ray.origin, _ray.direction * _maxDistanceRay, Color.green);
-            Rich1.GetComponent<Outline>().OutlineWidth = 4;
-            Rich2.GetComponent<Outline>().OutlineWidth = 4;
             if(Input.GetKeyDown(KeyCode.E))
             {
 
@@ -50,10 +48,50 @@ public class Interactive : MonoBehaviour
                 DIMON.GetComponent<DemonDoor>().Open();
             }
         }
+    }
+
+
+    private Outline outline;
+
+    private void OutLining()
+    {
+        if(_hit.transform != null && _hit.transform.GetComponent<Outline>())
+        {
+            outline = _hit.transform.GetComponent<Outline>();
+            outline.GetComponent<Outline>().OutlineWidth = 4;
+        }
         else
         {
-            Rich1.GetComponent<Outline>().OutlineWidth = 0;
-            Rich2.GetComponent<Outline>().OutlineWidth = 0;
+            outline.GetComponent<Outline>().OutlineWidth = 0;
         }
+    }
+
+
+    public GameObject hand;
+    GameObject current_Item;
+    bool canPickUp;
+
+    void Pick_Item()
+    {
+        if(Physics.Raycast(_ray, out _hit,_maxDistanceRay))
+        {
+            if(_hit.transform.tag == "ThingToPick")
+            {
+                if(canPickUp) Drop();
+                current_Item = _hit.transform.gameObject;
+                current_Item.GetComponent<Rigidbody>().isKinematic = true;
+                current_Item.transform.parent = hand.transform;
+                current_Item.transform.localPosition = Vector3.zero;
+                current_Item.transform.localEulerAngles = new Vector3(172f,270f,93f);
+                canPickUp = true;
+            }
+        }
+    }
+    void Drop()
+    {
+        current_Item.transform.parent = null;
+        current_Item.GetComponent<Rigidbody>().isKinematic = false;
+        canPickUp = false;
+        current_Item = null;
     }
 }
