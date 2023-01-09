5b48bd3066e61fd173ec16ff1b2a7524e4726c91
Adam <adam.butakov@yandex.ru>
2023-01-10 02:25:42 +0300

    РУКА БЛУД

---
+++ billy bones/Assets/Scripts/HandMove.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HandMove : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+
+    private NavMeshAgent myAgent;
+
+    private Animator anim;
+
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(myRay, out hitInfo,100, whatCanBeClickedOn))
+            {
+                myAgent.enabled = true;
+                myAgent.speed = 5;
+                myAgent.acceleration = 1000;
+                myAgent.SetDestination(hitInfo.point);
+                anim.SetBool("IsRunning", true);
+            } 
+        }
+        else
+        {
+           myAgent.speed = 0;
+           myAgent.enabled = false;
+           anim.SetBool("IsRunning", false);
+           anim.SetBool("Failed", false);
+        }
+    }
+}
---
+++ billy bones/Assets/Scripts/HandRemove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandRemove : MonoBehaviour
+{
+    public GameObject LeftHand;
+    public GameObject SpawnPoint;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = LeftHand.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            LeftHand.transform.parent = SpawnPoint.transform;
+            LeftHand.transform.localPosition = Vector3.zero;
+            LeftHand.transform.localEulerAngles = new Vector3(0f,0f,0f);
+            LeftHand.transform.parent = null;
+            anim.SetBool("Failed", true);
+        }
+    }
+}
