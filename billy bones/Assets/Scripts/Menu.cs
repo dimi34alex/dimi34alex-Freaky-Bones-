6c0255dd0ccabc01c8ff93fedeab72f005ebd905
FandXOfficial <90962224+FandXOfficial@users.noreply.github.com>
2023-01-11 19:08:10 +0300

    какаха

--- billy bones/Assets/Scripts/Menu.cs
+++ billy bones/Assets/Scripts/Menu.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject MainMenu, Settings, Loading;
+    public GameObject MainMenu, Settings, Loading, LogoFront, Logo, LogoBack;
     public bool Quit_Press = false, Pause_Press = false;
+    public int count = 0;
     private Animator anim;
+
+    public void Logo_f()
+    {
+        Logo.SetActive(true);
+        Invoke("MainMenu_f", 5f);
+    }
 
     public void MainMenu_f()
     {
+        Logo.SetActive(false);
+        LogoBack.SetActive(false);
+        Invoke("LogoFront_off", 1f);
         MainMenu.SetActive(true);
         Settings.SetActive(false);
         Loading.SetActive(false);
         anim.SetBool("IsRunning", false);
+    }
+
+    public void LogoFront_off()
+    {
+        LogoFront.SetActive(false);
     }
 
     public void Settings_f()
@@ -47,10 +63,32 @@ public class Menu : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        Logo_f();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        if (LogoBack.GetComponent<Image>().color.a <= 1 && LogoBack.GetComponent<Image>().color.a > 0)
+        {
+            var tempColor = LogoBack.GetComponent<Image>().color;
+            tempColor.a -= Time.fixedDeltaTime / 3;
+            LogoBack.GetComponent<Image>().color = tempColor;
+        }
+
+        if (LogoBack.GetComponent<Image>().color.a < 0.001)
+        {
+            var tempColor = Logo.GetComponent<Image>().color;
+            tempColor.a -= Time.fixedDeltaTime / 2;
+            Logo.GetComponent<Image>().color = tempColor;
+        }
+
+        if (Logo.GetComponent<Image>().color.a < 0.001)
+        {
+            var tempColor = LogoFront.GetComponent<Image>().color;
+            tempColor.a -= Time.fixedDeltaTime;
+            LogoFront.GetComponent<Image>().color = tempColor;
+        }
+
         if (Quit_Press)
         {
             Application.Quit();
