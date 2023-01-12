97b63b2a3968bff38c0518e39d31b296151bf85c
dimi34alex <dimi_alex@mail.ru>
2023-01-12 19:53:45 +0300

    add stamina

--- billy bones/Assets/Scripts/PlayerMove.cs
+++ billy bones/Assets/Scripts/PlayerMove.cs
@@ -6,12 +6,10 @@ using UnityEngine.AI;
 public class PlayerMove : MonoBehaviour
 {
     public LayerMask whatCanBeClickedOn;
-
+    public float costRunStamina = 0.1f;
     private NavMeshAgent myAgent;
-
+    float timerStaminaRun = 0.0f;
     private Animator anim;
-
-    public bool HasTorch = false;
 
     void Start()
     {
@@ -21,53 +19,49 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        if(HasTorch == false)
+        if (Input.GetMouseButton(0))
         {
-            anim.SetBool("HasTorch", false);
-            if (Input.GetMouseButton(0))
-            {
-                Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hitInfo;
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
 
-                if(Physics.Raycast(myRay, out hitInfo,100, whatCanBeClickedOn))
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgent.enabled = true;
+                myAgent.speed = 5;
+                myAgent.acceleration = 1000;
+                myAgent.SetDestination(hitInfo.point);
+                anim.SetBool("IsRunning", true);
+            }
+        }
+        else if (Input.GetMouseButton(1))
+        {
+
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                if (StaminaBar.instance.staminaBar.value >= 1)
                 {
+                    Debug.Log("ÀÂÇÀÛÂÀÛÕÂÀÕ");
+                    if (timerStaminaRun >= costRunStamina)
+                    {
+                        StaminaBar.instance.UseStamina(1);
+                        timerStaminaRun = 0.0f;
+                    }
+                    myAgent.speed = 10;
+                    timerStaminaRun += Time.fixedDeltaTime;
                     myAgent.enabled = true;
-                    myAgent.speed = 5;
                     myAgent.acceleration = 1000;
                     myAgent.SetDestination(hitInfo.point);
                     anim.SetBool("IsRunning", true);
-                } 
-            }
-            else
-            {
-                myAgent.speed = 0;
-                myAgent.enabled = false;
-                anim.SetBool("IsRunning", false);
+                }
             }
         }
-        if(HasTorch == true)
+        else
         {
-            anim.SetBool("HasTorch", true);
-            if (Input.GetMouseButton(0))
-            {
-                Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hitInfo;
-
-                if(Physics.Raycast(myRay, out hitInfo,100, whatCanBeClickedOn))
-                {
-                    myAgent.enabled = true;
-                    myAgent.speed = 5;
-                    myAgent.acceleration = 1000;
-                    myAgent.SetDestination(hitInfo.point);
-                    anim.SetBool("TorchRunning", true);
-                } 
-            }
-            else
-            {
-                myAgent.speed = 0;
-                myAgent.enabled = false;
-                anim.SetBool("TorchRunning", false);
-            }
+            myAgent.speed = 0;
+            myAgent.enabled = false;
+            anim.SetBool("IsRunning", false);
         }
     }
 }
