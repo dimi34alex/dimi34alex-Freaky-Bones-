44dc91fa32ebd39f1cea24d062612334a732a41e
Adam <adam.butakov@yandex.ru>
2023-01-13 21:40:51 +0300

    универсальные ричаги

--- billy bones/Assets/Scripts/doorManager.cs
+++ billy bones/Assets/Scripts/doorManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class doorManager : MonoBehaviour
 {
-    private bool _isOpened = true;
+    private bool _isOpened = false;
     [SerializeField] private Animator _animator;
 
 
     public void Open()
     {
+        _isOpened = true;
         _animator.SetBool("isOpened",_isOpened);
-        _isOpened = !_isOpened;
     }
 }
