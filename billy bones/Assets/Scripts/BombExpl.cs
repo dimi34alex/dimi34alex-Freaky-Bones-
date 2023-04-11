db5ecb6103ef89dfb4958e86cd3a744b97fa84ef
dimi34alex <dimi_alex@mail.ru>
2023-04-11 19:11:59 +0300

    бомба взрывается один раз и уничтожается

--- billy bones/Assets/Scripts/BombExpl.cs
+++ billy bones/Assets/Scripts/BombExpl.cs
@@ -6,26 +6,22 @@ public class BombExpl : MonoBehaviour
 {
     public GameObject bomb;
     public GameObject explosion;
-    readonly Explosion_preferens exp_pr;
-    
-    public bool is_explosion = false;
+
+    ParticleSystem explosionPS;
     GameObject bomb_clone;
-    GameObject explosion_clone;
-    // Start is called before the first frame update
     void Start()
     {
         bomb_clone = bomb;
+        explosionPS = explosion.GetComponent<ParticleSystem>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            explosion_clone = Instantiate(explosion, bomb.transform.position, Quaternion.identity);
-            exp_pr.IsExplosion(explosion_clone);
+            GameObject explosion_clone = Instantiate(explosion, bomb.transform.position, Quaternion.identity);
             Destroy(bomb_clone);
-            Debug.Log("Дошел до конца");
-                        
+            Destroy(explosion_clone, explosionPS.main.duration);       
         }
     }
 }
