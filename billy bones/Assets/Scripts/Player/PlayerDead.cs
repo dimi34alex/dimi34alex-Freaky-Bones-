307de057253702fbc76a40041a84606d6dca43ab
Adam <adam.butakov@yandex.ru>
2023-06-16 02:02:55 +0300

    Взрыв костяшек молофьяшек

---
+++ billy bones/Assets/Scripts/Player/PlayerDead.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDead : MonoBehaviour
+{
+    public GameObject Skeleton;
+ 
+    public bool Dead = false;
+
+    public GameObject Head;
+    public GameObject Body;
+    public GameObject LHand;
+    public GameObject RHand;
+    public GameObject Legs;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            Skeleton.SetActive(false);
+
+            Head.transform.parent = null;
+            Head.GetComponent<Rigidbody>().isKinematic = false;
+            Head.GetComponent<Collider>().enabled = true;
+            Head.SetActive(true);
+
+            Body.transform.parent = null;
+            Body.GetComponent<Rigidbody>().isKinematic = false;
+            Body.GetComponent<Collider>().enabled = true;
+            Body.SetActive(true);
+
+            LHand.transform.parent = null;
+            LHand.GetComponent<Rigidbody>().isKinematic = false;
+            LHand.GetComponent<Collider>().enabled = true;
+            LHand.GetComponent<Animator>().enabled = false;
+            LHand.SetActive(true);
+
+            RHand.transform.parent = null;
+            RHand.GetComponent<Rigidbody>().isKinematic = false;
+            RHand.GetComponent<Collider>().enabled = true;
+            RHand.GetComponent<Animator>().enabled = false;
+            RHand.SetActive(true);
+
+            Legs.transform.parent = null;
+            Legs.GetComponent<Rigidbody>().isKinematic = false;
+            Legs.GetComponent<Collider>().enabled = true;
+            Legs.SetActive(true);
+
+            
+        }
+    }
+}
